642f33337cc247d75182ed394a33d6d8d565d4eb
Sushmithagn04 <150269798+Sushmithagn04@users.noreply.github.com>
2024-04-20 08:54:20 +0530

    Update SimpleInterest.py

--- SimpleInterest.py
+++ SimpleInterest.py
@@ -2,8 +2,8 @@ def simpleInterest(principal,rate,time):
     result=(principal*rate*time)/100
     return result
 
-principal=int(input("Enter the principal amount : "))
-rate=int(input("Enter the rate : "))
-time=int(input("Enter the time : "))
+principal=float(input("Enter the principal amount : "))
+rate=float(input("Enter the rate : "))
+time=float(input("Enter the time : "))
 
 print("Simple Interest : ", simpleInterest(principal,rate,time))
